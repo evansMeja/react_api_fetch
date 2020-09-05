f2b6cb15f4e739287c30b25ce270f51d1d3da12f
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-09-05 04:50:00 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import './jquery-3.5.1.min.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component { 
+  state = {
+    loading: true,
+    jsonData: null, 
+  };
+
+
+  async componentDidMount () { 
+    const url = "https://api.jsonbin.io/b/5f535c74514ec5112d16b12b/1" 
+    const response = await fetch(url,{headers : {"secret-key":"$2b$10$RQO02/Ctj0ySqojfqfFKAuu3zdMaCY1oYLQSj.XRZRMkR54vReUdu"}}); 
+    const data = await response.json();
+    this.setState({ jsonData: data, loading:false}); 
+  } 
+
+  load_data(x) {
+    if (this.state.loading){
+      x.innerHTML = " loading";
+    }else{
+      x.innerHTML = " loaded";
+      var my_string = JSON.stringify(this.state.jsonData, null, 4)
+      x.innerHTML = my_string;
+      //console.log(this.state.jsonData['address'][0]['address_line'])
+    }
+  }
+
+  render() {
+    return ( 
+      <div> 
+        <div className="container justify-content-center">
+          <h4 className='p-2 bg-primary text-white m-3 text-center rounded-pill'>Simple App to load React API Data</h4>
+          <div className="form-group">
+            <button className="btn btn-info form-control" onClick={() => this.load_data(document.getElementById("result"))}>Load Data</button>
+          </div>
+          <div className="text" id="result"></div>
+        </div> 
+      </div> 
+    );
+  };
+
 }
-
-export default App;
